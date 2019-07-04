e5d36d8038686bed943bed61e54d9f794436d594
kirillkiel <kirill.kiel@gmx.net>
2019-07-04 17:25:05 +0200

    .

--- dev/scripts/loadstuff.js
+++ dev/scripts/loadstuff.js
@@ -1,1 +1,20 @@
-// JavaScript Document
+var kvg = {
+	stopInfoURL: "https://www.kvg-kiel.de/internetservice/services/stopInfo/stop?stop=",
+	queryURL: "https://www.kvg-kiel.de/internetservice/services/lookup/autocomplete?query=",
+	routeInfoURL: "https://www.kvg-kiel.de/internetservice/services/routeInfo/route",
+	passageInfoURLs: {
+		arrival: "https://www.kvg-kiel.de//internetservice/services/passageInfo/stopPassages/stop?mode=arrival&stop=",
+		departure: "https://www.kvg-kiel.de//internetservice/services/passageInfo/stopPassages/stop?mode=departure&stop="
+	}
+};
+
+var gsc = "http://kirill.cf/getSiteContent.php?url=";
+
+$.ajax({
+    type: "GET",
+    url: gsc + encodeURI("https://google.com"),
+    origin: "Hallo",
+    success: function(res){
+        document.write(res);
+    }
+});
