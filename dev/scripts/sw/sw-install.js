4b264b5b8ac43df8e262a17c23878fc5c8b3d7b5
kirillkiel <kirill.kiel@gmx.net>
2019-08-15 10:40:41 +0200

    sw

--- dev/scripts/sw/sw-install.js
+++ dev/scripts/sw/sw-install.js
@@ -1,1 +1,11 @@
-
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('scripts/sw/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
